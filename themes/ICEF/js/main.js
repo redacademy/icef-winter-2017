b91214bcdb3bbbaddbd76b6f669767f4e72b2e3a
andrew.nelsonrd@gmail.com <andrewnelson@Andrews-MacBook-Pro.local>
2017-03-14 13:44:25 -0700

    fixed header

--- themes/ICEF/js/main.js
+++ themes/ICEF/js/main.js
@@ -2,7 +2,7 @@
 
     $('.fa-bars').on('click', function(event){
         event.preventDefault();
-        $('.menu, .yellow-dot-filler').toggleClass('show-menu')
+        $('.menu, .yellow-dot-filler').toggleClass('show-menu');
 
     })
 
