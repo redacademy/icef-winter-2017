44c9078bce587ad2d3ba8c9f235a1d4484060f6f
andrew.nelsonrd@gmail.com <andrewnelson@Andrews-MacBook-Pro.local>
2017-03-14 15:57:48 -0700

    did hero banner on mobile home page

--- themes/ICEF/front-page.php
+++ themes/ICEF/front-page.php
@@ -7,7 +7,16 @@
 
 get_header(); ?>
 
+<header class="front-page-head">
+    <div class='home-hero'></div>
+    <h2>One Community At a Time</h2>
+    <p>Our vision is a community that can survive holistically 
+        and self-sustainably.</p>
+    <img src='images/svg/scroll-down.svg' rel='scroll-down' />
+</header>
 
+<section class="whoWeAre">
+</section>
 
 
 <?php get_footer(); ?>
