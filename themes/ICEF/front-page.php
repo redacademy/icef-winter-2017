316161fe0dd6f38c212577cba8c1882b80201b42
andrew.nelsonrd@gmail.com <andrewnelson@Andrews-MacBook-Pro.local>
2017-03-15 12:39:37 -0700

    added paragraph text on front page

--- themes/ICEF/front-page.php
+++ themes/ICEF/front-page.php
@@ -18,10 +18,19 @@ get_header(); ?>
 <section class="who-we-are">
     <h3>Who We Are</h3>
     <img class='cow-farmer' src='<?php echo get_template_directory_uri();?>/images/jpegs/home-who-we-are.jpg' alt='cow-farmer' />
-    <div class="key-words"><p>International<br>Community<br>Empowerment<br>Foundation</p></div>
+    <div class="key-words"><p>International<br><span>Community<br>Empowerment</span><br>Foundation</p></div>
+    <p class="who-we-are-text">We work with communities in Uganda to support their well being and sustainable growth. Our focus is on holistic development,
+        knowing that the only way to truly support a community is through offering 
+        multiple interconnected programs. We stay with the community for as long as 
+        it takes for them to achieve self-sustainability. The community drives this
+        progressive change, as key stakeholders in all planning, implementation, monitoring and evaluation of our work. 
+        No decisions are made without the input of our community. 
+    </p>
 </section>
 </div>
 </div>
+
+<?php get_footer(); ?>
 
 
 
