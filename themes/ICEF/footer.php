4e01951fa4ec6340093cb9faaeee829fc39a588d
julielo5 <julielo.inbox@gmail.com>
2017-03-15 16:34:01 -0700

    Styled contents of footer.

--- themes/ICEF/footer.php
+++ themes/ICEF/footer.php
@@ -16,19 +16,19 @@
 							<img src="<?php echo get_template_directory_uri();?>/images/svg/canada-footer.svg" alt="Image of Canada"><span class = "footer-text">Canada</span>
 						</div>							
 						<div class ="canada-info">
-							<p><span class = "footer-text">Email:</span> info@icefcanada.org</p>
-							<p><span class = "footer-text">Phone:</span> +778-899-9448</p>
-							<p><span class = "footer-text">Address:</span> 5730 Sherwood Blvd</p>
-							<p>Delta, BC, V4L2C6</p>
+							<p><i class="fa fa-envelope" aria-hidden="true"></i>info@icefcanada.org</p>
+							<p><i class="fa fa-phone" aria-hidden="true"></i>+778-899-9448</p>
+							<p><i class="fa fa-map-marker" aria-hidden="true"></i><span class="address-line">5730 Sherwood Blvd</p>
+							<p><span class="address-line2">Delta, BC, V4L2C6</span></p>
 						</div> <!-- .canada-info -->
 						<div class = "uganda-image">
 							<img src="<?php echo get_template_directory_uri();?>/images/svg/uganda-footer.svg" alt="Image of Uganda"><span class = "footer-text">Uganda</span>
 						</div>
 						<div class = "uganda-info">
-							<p><span class = "footer-text">Phone:</span> +256 0701 589 765</p>
-								<span class = "footer-text">Address:</span> <span class = "address">Tekera Resource Centre
-								<p>P.O. Box 960</p>
-								<p>Masaka, Uganda</p>
+							<p><i class="fa fa-phone" aria-hidden="true"></i>+256 0701 589 765</p>
+								<p><i class="fa fa-map-marker" aria-hidden="true"></i><span class="address-line">Tekera Resource Centre</p>
+								<p><span class="address-line2">P.O. Box 960</span></p>
+								<p><span class="address-line2">Masaka, Uganda</span></p>
 							</span>
 						</div> <!-- .uganda-info" -->
 					</div> <!-- .footer-wrapper -->
