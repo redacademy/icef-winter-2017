fc7ca1234d0019a6e1be70ce513f3925a716af72
Johnwat92 <JohnWat92@gmail.com>
2017-03-15 11:05:02 -0700

    made changes to approach and page php

--- themes/ICEF/page.php
+++ themes/ICEF/page.php
@@ -13,9 +13,15 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+						
 
 			<?php endwhile; // End of the loop. ?>
-
+	<?php echo CFS()->get('approach_hero');?>
+			<?php echo CFS()->get('below_hero_text');?>
+			<?php echo CFS()->get('below_self_sustainability');?>
+			<?php echo CFS()->get('below_quote');?>
+			<?php echo CFS()->get('last_picture');?>
+			<?php echo CFS()->get('below_last_picture_text');?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
