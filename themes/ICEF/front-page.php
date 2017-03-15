fce7de840cb523513f1b84a49ffa0c0f3ffc7001
andrew.nelsonrd@gmail.com <andrewnelson@Andrews-MacBook-Pro.local>
2017-03-15 08:26:46 -0700

    added the scroll down image, and fixed hero position on homepage

--- themes/ICEF/front-page.php
+++ themes/ICEF/front-page.php
@@ -12,7 +12,7 @@ get_header(); ?>
     <h2>One Community At a Time</h2>
     <p>Our vision is a community that can survive holistically 
         and self-sustainably.</p>
-    <img src='../../images/svg/scroll-down.svg' rel='scroll-down' />
+    <img class="scroll-down" src='<?php echo get_template_directory_uri();?>/images/svg/scroll-down.svg' alt='scroll-down' />
 </header>
 
 <section class="whoWeAre">
