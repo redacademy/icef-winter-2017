8d837601c328260ab44f5246b016eda7aa74ea06
Johnwat92 <JohnWat92@gmail.com>
2017-03-16 11:33:39 -0700

    before pull

--- themes/ICEF/internship.php
+++ themes/ICEF/internship.php
@@ -11,10 +11,15 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<img src="<?php echo CFS()->get('internship_first_image');?>" alt="top image">
 			<?php echo CFS()->get('internship_below_top_image');?>
-			<?php echo CFS()->get('internship_below_second_image_text');?>
 			<img src="<?php echo CFS()->get('internship_second_image');?>" alt="top image">
+			<div class = "yellowBackground">
+				<?php echo CFS()->get('internship_below_second_image_title_text');?>
+				<?php echo CFS()->get('internship_below_second_image_text');?>
+			</div>
 			<?php echo CFS()->get('internship_position_availability');?>
-			<?php echo CFS()->get('internship_position_name');?>
+			<div class = "yellowBackground">
+				<?php echo CFS()->get('internship_position_name');?>
+			</div>
 			<?php echo CFS()->get('internship_position_term');?>
 			<?php echo CFS()->get('internship_position_description');?>
 			<?php echo CFS()->get('internship_position_requirements');?>
