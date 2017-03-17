003ffe3bc5048444f9d8a69c2c97e2c265da6f41
julielo5 <julielo.inbox@gmail.com>
2017-03-16 18:10:09 -0700

    Added and styled links in footer.

--- themes/ICEF/footer.php
+++ themes/ICEF/footer.php
@@ -17,10 +17,10 @@
 								<img src="<?php echo get_template_directory_uri();?>/images/svg/canada-footer.svg" alt="Image of Canada"><span class = "footer-text">Canada</span>
 							</div>												
 							<div class ="canada-info">
-								<p><i class="fa fa-envelope" aria-hidden="true"></i>info@icefcanada.org</p>
-								<p><i class="fa fa-phone" aria-hidden="true"></i>+778-899-9448</p>
-								<p><i class="fa fa-map-marker" aria-hidden="true"></i><span class="address-line">5730 Sherwood Blvd</p>
-								<p><span class="address-line2">Delta, BC, V4L2C6</span></p>
+								<p><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:info@inhabitent.com">info@icefcanada.org</a></p>
+								<p><i class="fa fa-phone" aria-hidden="true"></i><a href="tel:778-899-9448">+778-899-9448</a></p>
+								<a href="https://goo.gl/maps/fyMRKhSa7UJ2"><p><i class="fa fa-map-marker" aria-hidden="true"></i><span class="address-line">5730 Sherwood Blvd</p>
+								<p><span class="address-line2">Delta, BC, V4L2C6</span></p></a>
 							</div> <!-- .canada-info -->
 						</div>
 						<div class ="uganda-wrapper">
@@ -28,11 +28,14 @@
 								<img src="<?php echo get_template_directory_uri();?>/images/svg/uganda-footer.svg" alt="Image of Uganda"><span class = "footer-text">Uganda</span>
 							</div>
 							<div class = "uganda-info">
-								<p><i class="fa fa-phone" aria-hidden="true"></i>+256 0701 589 765</p>
-									<p><i class="fa fa-map-marker" aria-hidden="true"></i><span class="address-line">Tekera Resource Centre</p>
-									<p><span class="address-line2">P.O. Box 960</span></p>
-									<p><span class="address-line2">Masaka, Uganda</span></p>
-								</span>
+								<p><i class="fa fa-facebook" aria-hidden="true"></i><a href="https://www.facebook.com/tekeraresourcecentre/Facebook">Facebook</a></p>
+								<p><i class="fa fa-phone" aria-hidden="true"></i><a href="tel:256-0701-589-765">+256 0701 589 765</a></p>
+								<p><i class="fa fa-map-marker" aria-hidden="true"></i>
+									<span class="address-line-uganda"><a href="https://goo.gl/maps/9t6tjmJuCGo">Tekera Resource Centre</p>
+										<p><span class="address-line2">P.O. Box 960</span></p>
+										<p><span class="address-line2">Masaka, Uganda</span></p></a>
+									</span> <!-- .address-line -->
+								</p>
 							</div> <!-- .uganda-info" -->
 						</div>
 					</div> <!-- .footer-wrapper -->
