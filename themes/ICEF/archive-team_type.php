2b1d1db4ff85d9a55a72f443def5509e52d9bbed
andrew.nelsonrd@gmail.com <andrewnelson@Andrews-MacBook-Pro.local>
2017-03-19 13:07:17 -0700

    added for loops to get the correct border for team_type

--- themes/ICEF/archive-team_type.php
+++ themes/ICEF/archive-team_type.php
@@ -14,9 +14,27 @@ get_header(); ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 
 				<div class='team-member'>
-					<img class='profile-pic' src='<?php echo CFS()->get( 'profile_picture' ); ?>'  />
-					<?php echo CFS()->get( 'name' ); ?>
-					<?php echo CFS()->get( 'job' ); ?>	
+
+					<?php
+					$job = CFS()->get( 'job' );
+					$country = get_the_category();
+
+					if($job == 'President'){?>
+						<img src='<?php echo get_template_directory_uri();?>/images/svg/deco-president.svg' class='job-border' />
+					<?php } else if($job == 'Executive Director'){ ?>
+						<img src='<?php echo get_template_directory_uri();?>/images/svg/deco-exec-director.svg' class='job-border' />
+					<?php } else if($country == 'Uganda'){ ?>
+						<img src='<?php echo get_template_directory_uri();?>/images/svg/deco-uganda.svg' class='job-border' />
+					<?php } else { ?>
+						<img src='<?php echo get_template_directory_uri();?>/images/svg/deco-director.svg' class='job-border' />
+					<?php } ?>
+					
+					<!--//-->
+					<div class='profile-pic'>
+						<img  src='<?php echo CFS()->get( 'profile_picture' ); ?>'  />
+					</div>
+					<p class='name' ><?php echo CFS()->get( 'name' ); ?></p>
+					<p class='job' ><?php echo $job; ?></p>
 				</div>
 
 				<?php endwhile; // End of the loop. ?>
