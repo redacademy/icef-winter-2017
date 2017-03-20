439b0c3a6c091e6e6a21432de8866ef3dafb2178
andrew.nelsonrd@gmail.com <andrewnelson@Andrews-MacBook-Pro.local>
2017-03-20 09:54:01 -0700

    changed style.css merge

--- themes/ICEF/approach.php
+++ themes/ICEF/approach.php
@@ -9,37 +9,52 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			
-			<!--<?php the_content();?>-->
-			<img src="<?php echo CFS()->get('approach_hero');?>" alt=:"top image">
-			<!--<img src="<?php echo get_template_directory_uri();?>/images/jpegs/our-approach-title2x.jpg" alt="top image">-->
+			<section class="approachTitle">
+				<h1><?php the_title();?></h1>
+			</section>
+			<section class="hero">
+				<img src="<?php echo CFS()->get('approach_hero');?>" alt=:"top image">
+			</section>
 			<?php echo CFS()->get('below_hero_text');?>
-			<img class ="mobile-swirl" src="<?php echo get_template_directory_uri();?>/images/svg/mobile-swirl.svg" alt="swirly image">
-			<!--<img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-mobile.svg" alt="swirly image">-->
-			<ul class = "mobile-approach-table">
-				<li> <img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-icons/hand-heart.svg" alt="Hand-Heart-Icon"><p><span class = "tableNumbers">01.</span> Support</p>
-				</li>
-				<li> <img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-icons/tractor.svg" alt="Tractor-Icon"><p><span class = "tableNumbers">02.</span> Economic</p>
-				</li>
-				<li> <img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-icons/cap-mob.svg" alt="Cap-mob-Icon"><p><span class = "tableNumbers">03.</span> Education</p>
-				</li>
-				<li> <img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-icons/infrastructure.svg" alt="Infrastructure-Icon"><p><span class = "tableNumbers">04.</span> Infrastructure</p>
-				</li>
-				<li> <img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-icons/social-mob.svg" alt="Social-mob-Icon"><p><span class = "tableNumbers">05.</span> Social</p>
-				</li>
-				<li> <img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-icons/health-mob.svg" alt="Health-mob-Icon"><p><span class = "tableNumbers">06.</span> Health</p>
-				</li>
-				<li> <img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-icons/well-being.svg" alt="Well-being-Icon"><p><span class = "tableNumbers">07.</span> Well-being</p>
-				</li>
-			</ul>
-			<!--<img src="<?php echo get_template_directory_uri();?>/images/svg/Mobile_ApproachTable4.svg" alt ="table image">-->
+			<div class = "desktopSwirl">
+				<img class ="desktop-swirl" src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-wheel.svg" alt="our-approach-wheel">
+			</div>
+			<section class ="swirlAndText">
+				<div class = "mobileSwirl">
+					<img class ="mobile-swirl" src="<?php echo get_template_directory_uri();?>/images/svg/koru-swirl.svg" alt="swirly-image">
+					<div class = "swirlText">
+						<p> Self-Sustainability</p>
+					</div>
+				</div>
+				<ul class = "mobile-approach-table">
+					<li> <img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-icons/hand-heart.svg" alt="Hand-Heart-Icon"><p><span class = "tableNumbers">01.</span> Support</p>
+					</li>
+					<li> <img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-icons/tractor.svg" alt="Tractor-Icon"><p><span class = "tableNumbers">02.</span> Economic</p>
+					</li>
+					<li> <img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-icons/cap-mob.svg" alt="Cap-mob-Icon"><p><span class = "tableNumbers">03.</span> Education</p>
+					</li>
+					<li> <img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-icons/infrastructure.svg" alt="Infrastructure-Icon"><p><span class = "tableNumbers">04.</span> Infrastructure</p>
+					</li>
+					<li> <img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-icons/social-mob.svg" alt="Social-mob-Icon"><p><span class = "tableNumbers">05.</span> Social</p>
+					</li>
+					<li> <img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-icons/health-mob.svg" alt="Health-mob-Icon"><p><span class = "tableNumbers">06.</span> Health</p>
+					</li>
+					<li> <img src="<?php echo get_template_directory_uri();?>/images/svg/our-approach-icons/well-being.svg" alt="Well-being-Icon"><p><span class = "tableNumbers">07.</span> Well-being</p>
+					</li>
+				</ul>
+			</section>
 			<?php echo CFS()->get('below_self_sustainability');?>
+			<div class = "quoteBackground">
+				<?php echo CFS()->get('approach_quote');?>
+			</div>
 			<?php echo CFS()->get('below_quote');?>
-			<!--<?php echo CFS()->get('last_picture');?>-->
-			<img src="<?php echo CFS()->get('last_picture');?>" alt=:"last picture">
-			<!--<img src="<?php echo get_template_directory_uri();?>/images/jpegs/our-approach-bottom-mobile2x.jpg" alt="bottom image">-->
+			<div class="lastPictureMobile">
+				<img src="<?php echo CFS()->get('last_picture');?>" alt="last picture">
+			</div>
+			<div class="lastPictureDesktop">
+				<img src="<?php echo CFS()->get('last_picture_desktop');?>" alt="last-picture-desktop">
+			</div>
 			<?php echo CFS()->get('below_last_picture_text');?> 
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
--- themes/ICEF/donate.php
+++ themes/ICEF/donate.php
@@ -9,18 +9,21 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			
+			<img src="<?php echo CFS()->get('donate_landing_title_image');?>">	
+			<?php echo CFS()->get('donate_to_trc_title');?>
+			<?php echo CFS()->get('donation_to_trc_description');?>
+			<?php echo CFS()->get('donation_title');?>	
+
+			<?php echo CFS()->get('donation_description');?>
+			<img src="<?php echo CFS()->get('project_image');?>">
+			<?php echo CFS()->get('project_description');?>	
 			<div class="yellow-dots-banner">
 				<!--<img src="<?php echo get_template_directory_uri();?>/images/svg/yellow-dots.svg" alt="image of yellow dots on white background">-->
 			</div>
-			<?php echo CFS()->get('donation_description');?>	
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
+			
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
