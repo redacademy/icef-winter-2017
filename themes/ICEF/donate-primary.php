cbef05d4531c210a7ef5ce2d55bbc24dc6dc231d
andrew.nelsonrd@gmail.com <andrewnelson@Andrews-MacBook-Pro.local>
2017-03-21 13:43:24 -0700

    merged

--- themes/ICEF/donate-primary.php
+++ themes/ICEF/donate-primary.php
@@ -19,7 +19,41 @@ get_header(); ?>
 					<p class="donate-trc-desc"><?php echo CFS()->get('primary_school_description');?></p>
 					<img src="<?php echo CFS()->get('primary_school_image');?>">
 				</div>
+				<div class="process-wrapper">
+					<h2><?php echo CFS()->get('primary_school_process_title');?></p>
+					<p class="primary_school_process_desc"><?php echo CFS()->get('primary_school_process_description');?></p>
+				</div>
+				<img src="<?php echo get_template_directory_uri();?>/images/svg/line.svg" alt="Image of a line">
+				<div class="primary-school-wrapper">
+					<img src="<?php echo get_template_directory_uri();?>/images/svg/page.svg" alt="Image of an open book">
+					<h2><?php echo CFS()->get('primary_school_scholarship_title');?><h2>
+					<p><?php echo CFS()->get('primary_school_scholarship_description');?></p>
 			</div>
+
+			<table>
+				<thead>
+				<tr>
+					<th>Scholarship</th>
+					<th>Amount</th>
+					<th>Donation Amount(CAD)</th>
+				</tr> 
+				</thead>
+				<tbody>
+					<?php $fields = CFS()->get( 'primary_school_scholarship_loop'); ?>
+					<?php foreach( $fields as $field ): ?>
+						<tr>
+							<td> <?php echo $field['scholarship_category']; ?> </td>
+							<td> <?php echo $field['scholarship_amount']; ?> </td>
+							<td> <?php echo $field['donation_amount']; ?> </td>
+						</tr>
+					<?php endforeach; ?>
+				</tbody>
+				<tfooter>* Price in yellow are in Canadian (CAD)</tfooter>
+			</table>
+			<p><?php echo CFS()->get('primary_school_student_fees');?></p>
+			<h2><?php echo CFS()->get('primary_school_sponsor_title');?></h2>
+			<p><?php echo CFS()->get('primary_school_sponsor_package');?></p>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
--- themes/ICEF/donate-vocational.php
+++ themes/ICEF/donate-vocational.php
@@ -12,14 +12,47 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<div class="container">
+				<div class="vocational-wrapper">
+					<h2><?php echo CFS()->get('vocational_school_title');?></h2>
+					<p class="donate-trc-desc"><?php echo CFS()->get('vocational_school_description');?></p>
+					<img src="<?php echo CFS()->get('vocational_school_image');?>">
+				</div>
+				<div class="process-wrapper">
+					<h2><?php echo CFS()->get('vocational_school_process_title');?></p>
+					<p class="vocational_school_process_desc"><?php echo CFS()->get('vocational_school_process_description');?></p>
+				</div>
+				<img src="<?php echo get_template_directory_uri();?>/images/svg/line.svg" alt="Image of a line">
+				<div class="vocational-school-wrapper">
+					<img src="<?php echo get_template_directory_uri();?>/images/svg/craft.svg" alt="Image of a spool of thread">
+					<h2><?php echo CFS()->get('vocational_school_scholarship_title');?><h2>
+					<p><?php echo CFS()->get('vocational_school_scholarship_description');?></p>
+			</div>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-						
-
-			<?php endwhile; // End of the loop. ?>
+			<table>
+				<thead>
+				<tr>
+					<th>Scholarship</th>
+					<th>Amount</th>
+					<th>Donation Amount(CAD)</th>
+				</tr> 
+				</thead>
+				<tbody>
+					<?php $fields = CFS()->get( 'vocational_school_scholarship_loop'); ?>
+					<?php foreach( $fields as $field ): ?>
+						<tr>
+							<td> <?php echo $field['scholarship_category']; ?> </td>
+							<td> <?php echo $field['scholarship_amount']; ?> </td>
+							<td> <?php echo $field['donation_amount']; ?> </td>
+						</tr>
+					<?php endforeach; ?>
+				</tbody>
+				<tfooter>* Price in yellow are in Canadian (CAD)</tfooter>
+			</table>
+			<p><?php echo CFS()->get('vocational_school_student_fees');?></p>
+			<h2><?php echo CFS()->get('vocational_school_sponsor_title');?></h2>
+			<p><?php echo CFS()->get('vocational_school_sponsor_package');?></p>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
