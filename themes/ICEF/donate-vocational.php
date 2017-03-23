f9b6df1d35538c27fb0bdf8a6b1bfc31ca825146
julielo5 <julielo.inbox@gmail.com>
2017-03-23 11:13:53 -0700

    Changed layout for process and sponsor sections of vocational school page for desktop.

--- themes/ICEF/donate-vocational.php
+++ themes/ICEF/donate-vocational.php
@@ -22,42 +22,46 @@ get_header(); ?>
 						<p class="vocational-school-desc"><?php echo CFS()->get('vocational_school_description');?></p>
 					</div>
 				</div>
-				<div class="process-wrapper">
-					<h2 class="vocational-school-process-title"><?php echo CFS()->get('vocational_school_process_title');?></h2>
-					<p class="vocational-school-process-desc"><?php echo CFS()->get('vocational_school_process_description');?></p>
-					<img src="<?php echo get_template_directory_uri();?>/images/svg/line.svg" alt="Image of a line">
-				</div>
+				<div class="content-wrapper">
+					<div class="process-wrapper">
+						<h2 class="vocational-school-process-title"><?php echo CFS()->get('vocational_school_process_title');?></h2>
+						<p class="vocational-school-process-desc"><?php echo CFS()->get('vocational_school_process_description');?></p>
+						<img src="<?php echo get_template_directory_uri();?>/images/svg/line.svg" alt="Image of a line">
+					</div> <!-- .process-wrapper -->
 				
-				<div class="vocational-scholarship-wrapper">
-					<div class="vocational-scholarship-heading">
-						<img src="<?php echo get_template_directory_uri();?>/images/svg/craft.svg" alt="Image of a spool of thread">
-						<h2 class="scholarship-title"><?php echo CFS()->get('vocational_school_scholarship_title');?><h2>
-					</div>
+				
+					<div class="vocational-scholarship-wrapper">
+						<div class="vocational-scholarship-heading">
+							<img src="<?php echo get_template_directory_uri();?>/images/svg/craft.svg" alt="Image of a spool of thread">
+							<h2 class="scholarship-title"><?php echo CFS()->get('vocational_school_scholarship_title');?><h2>
+						</div> <!-- .vocational-scholarship-heading -->
+						<div class="scholarship-content">
+							<p><?php echo CFS()->get('vocational_school_scholarship_description');?></p>
 
-					<p><?php echo CFS()->get('vocational_school_scholarship_description');?></p>
-
-					<table>
-						<!--<thead>
-						<tr>
-							<th>Scholarship</th>
-							<th>Amount</th>
-							<th>Donation Amount(CAD)</th>
-						</tr> 
-						</thead>-->
-						<tbody>
-							<?php $fields = CFS()->get( 'vocational_school_scholarship_loop'); ?>
-							<?php foreach( $fields as $field ): ?>
+							<table>
+								<!--<thead>
 								<tr>
-									<td> <?php echo $field['scholarship_category']; ?></td>
-									<td class="scholarship-amount"> <?php echo $field['scholarship_amount']; ?> </td>
-									<td class="donation-amount"> <?php echo $field['donation_amount']; ?> </td>
-								</tr>
-							<?php endforeach; ?>
-						</tbody>				
-					</table>
-					<p>* Prices in yellow are in Canadian (CAD)</p>
-					<p><?php echo CFS()->get('vocational_school_student_fees');?></p>
-				</div> <!-- vocational-scholarship-wrapper -->
+									<th>Scholarship</th>
+									<th>Amount</th>
+									<th>Donation Amount(CAD)</th>
+								</tr> 
+								</thead>-->
+								<tbody>
+									<?php $fields = CFS()->get( 'vocational_school_scholarship_loop'); ?>
+									<?php foreach( $fields as $field ): ?>
+										<tr>
+											<td> <?php echo $field['scholarship_category']; ?></td>
+											<td class="scholarship-amount"> <?php echo $field['scholarship_amount']; ?> </td>
+											<td class="donation-amount"> <?php echo $field['donation_amount']; ?> </td>
+										</tr>
+									<?php endforeach; ?>
+								</tbody>				
+							</table>
+							<p>* Prices in yellow are in Canadian (CAD)</p>
+							<p><?php echo CFS()->get('vocational_school_student_fees');?></p>
+						</div> <!-- .scholarship-content -->
+					</div> <!-- vocational-scholarship-wrapper -->
+				</div> <!-- .content-wrapper -->
 				<div class="sponsor-wrapper">
 					<div class="sponsor-section">	
 						<h2 class="vocational-school-sponsor-title"><?php echo CFS()->get('vocational_school_sponsor_title');?></h2>
@@ -67,8 +71,8 @@ get_header(); ?>
 								<li> <?php echo $item['sponsor_item']; ?> </li>
 							<?php endforeach; ?>
 						</ul>
-					</div>
-				</div>
+					</div> <!-- .sponsor-section -->
+				</div> <!-- .sponsor-wrapper -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
