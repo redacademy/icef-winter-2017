70a21a06b0c8612ff7117f36282812b910002e24
julielo5 <julielo.inbox@gmail.com>
2017-03-27 15:04:37 -0700

    Added page title to Donate page.

--- themes/ICEF/donate.php
+++ themes/ICEF/donate.php
@@ -10,8 +10,8 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<div class="container">
-				<div class="page-title">
-					<h1>Donations</h1>
+				<div class="page-title-wrapper">
+					<h1 class="page-title">Donations</h1>
 				</div>
 				<div class="img-wrapper">
 					<img src="<?php echo esc_html(CFS()->get('donate_landing_title_image'));?>">
